e87018eaf767a8c34f73fb8db76e164fac5475a7
scordier <sylvain.cordier@sysscale.com>
2021-03-03 10:29:58 +0100

    init project

--- src/components/request.js
+++
@@ -1,5 +0,0 @@
-export default function request(zipCode){
-  return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=YOURAPICODE`)
-          .then(response => response.json())
-          .then(data => data)
-}
